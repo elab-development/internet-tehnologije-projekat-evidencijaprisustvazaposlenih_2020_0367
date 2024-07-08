64902e37d636478ea8147785f0fac03fadcc6661
Hristina Tovladijac <hristina.tovladijac@gmail.com>
2024-07-09 00:26:42 +0200

    event component

--- attendance-system/src/components/events/CreateEventButton.js
+++ attendance-system/src/components/events/CreateEventButton.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
 
-function CreateEventButton() {
+import GlobalContext from '../../context/GlobalContext';
+
+const CreateEventButton = () => {
+  const { setShowEventModal } = useContext(GlobalContext);
+
   return (
-    <div>CreateEventButton</div>
-  )
-}
+    <div
+      onClick={() => setShowEventModal(true)}
+      className='border p-2 rounded-full flex cursor-pointer items-center shadow-md hover:shadow-2xl'
+    >
+      <span className='material-icons-outlined  text-gray-600 mx-2'>add</span>
+      <span>Create Event</span>
+    </div>
+  );
+};
 
-export default CreateEventButton
+export default CreateEventButton;
