75b1671056ef32c452c94946c6285e4256583aac
Hristina Tovladijac <hristina.tovladijac@gmail.com>
2024-07-12 15:13:50 +0200

    sidebar component

--- attendance-system/client/src/components/Sidebar.js
+++ attendance-system/client/src/components/Sidebar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react';
 
-const Sidebar = () => {
+import CreateEventButton from './events/CreateEventButton';
+import CreateCategoryButton from './category/CreateCategoryButton';
+import SmallCalendar from './calendar/SmallCalendar';
+import ViewMenu from './ViewMenu';
+import GlobalContext from '../context/GlobalContext';
+
+const Sidebar = ({ setView, view }) => {
+  const { loggedInUserData } = useContext(GlobalContext);
+
   return (
-    <div>Sidebar</div>
-  )
-}
+    <aside className='border p-5 w-64'>
+      <CreateEventButton />
+      {loggedInUserData?.role === 'admin' && <CreateCategoryButton />}
+      <SmallCalendar />
+      <ViewMenu setView={setView} view={view} />
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
