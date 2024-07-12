bc5ba88dd8381f29a2f2a2471ba2ea278ec7d2e3
Hristina Tovladijac <hristina.tovladijac@gmail.com>
2024-07-12 15:43:44 +0200

    calendar page

--- attendance-system/client/src/pages/Calendar.js
+++ attendance-system/client/src/pages/Calendar.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 
-function Calendar() {
+import { getMonth } from '../utils/calendarUtils';
+import GlobalContext from '../context/GlobalContext';
+import CalendarHeader from '../components/calendar/CalendarHeader';
+import Sidebar from '../components/Sidebar';
+import Month from '../components/calendar/Month';
+import AdminDashboard from '../components/admin/AdminDashboard';
+import EventModal from '../components/events/EventModal';
+import CategoryModal from '../components/category/CategoryModal';
+import DeskModal from '../components/desk/DeskModal';
+
+const Calendar = () => {
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  const [view, setView] = useState('month');
+
+  const { monthIndex, showEventModal, showCategoriesModal, showDeskModal } =
+    useContext(GlobalContext);
+
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
+
   return (
-    <div>Calendar</div>
-  )
-}
+    <>
+      <div className='h-screen flex flex-col bg-stone-50'>
+        <CalendarHeader />
+        <div className='flex flex-1'>
+          <Sidebar setView={setView} view={view} />
+          {view === 'month' && <Month month={currentMonth} />}
+          {view === 'admin' && <AdminDashboard />}
+        </div>
+      </div>
 
-export default Calendar
+      {showEventModal && <EventModal />}
+      {showCategoriesModal && <CategoryModal />}
+      {showDeskModal && <DeskModal />}
+    </>
+  );
+};
+
+export default Calendar;
